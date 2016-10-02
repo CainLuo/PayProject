01890502dbcd7aacc2a316d7cd3c5bee8ad9cd1a
CainLuo <zhebushimengfei@outlook.com>
2016-10-02 21:53:39 +0800

    完成ApplePay开发
    完成ApplePay开发

--- ApplePay-Objective-C/ApplePay-Objective-C/ViewController.m
+++ ApplePay-Objective-C/ApplePay-Objective-C/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <PassKit/PassKit.h>
 
-@interface ViewController ()
+@interface ViewController () <PKPaymentAuthorizationViewControllerDelegate>
 
 @end
 
@@ -16,12 +17,81 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)applePayAction:(UIButton *)sender {
+    
+    if ([PKPaymentAuthorizationViewController canMakePayments]) {
+        
+        NSLog(@"开始Apple Pay");
+
+        PKPaymentRequest *request = [[PKPaymentRequest alloc] init];
+        
+        PKPaymentSummaryItem *widgetOne = [PKPaymentSummaryItem summaryItemWithLabel:@"widget1"
+                                                                              amount:[NSDecimalNumber decimalNumberWithString:@"0.93"]];
+        
+        PKPaymentSummaryItem *widgetTwo = [PKPaymentSummaryItem summaryItemWithLabel:@"widget2"
+                                                                              amount:[NSDecimalNumber decimalNumberWithString:@"1.00"]];
+
+        PKPaymentSummaryItem *total = [PKPaymentSummaryItem summaryItemWithLabel:@"COMPANY NAME"
+                                                                              amount:[NSDecimalNumber decimalNumberWithString:@"1.93"]];
+
+        request.paymentSummaryItems = @[widgetOne, widgetTwo, total];
+        request.countryCode = @"US"; // 国家代码
+        request.currencyCode = @"USD"; // 货币代码
+        request.supportedNetworks = @[PKPaymentNetworkAmex,
+                                      PKPaymentNetworkChinaUnionPay,
+                                      PKPaymentNetworkDiscover,
+                                      PKPaymentNetworkInterac,
+                                      PKPaymentNetworkMasterCard,
+                                      PKPaymentNetworkPrivateLabel,
+                                      PKPaymentNetworkVisa];
+        
+        request.merchantIdentifier = @"merchant.com.pay.applepay.demo";
+        request.merchantCapabilities = PKMerchantCapabilityEMV;
+        request.requiredBillingAddressFields = PKAddressFieldEmail | PKAddressFieldPostalAddress;
+        
+        PKPaymentAuthorizationViewController *paymentAuthorizationController = [[PKPaymentAuthorizationViewController alloc] initWithPaymentRequest:request];
+        
+        paymentAuthorizationController.delegate = self;
+        
+        [self presentViewController:paymentAuthorizationController animated:YES completion:nil];
+    } else {
+        
+        NSLog(@"不支持Apple Pay");
+    }
+}
+
+// PKPaymentAuthorization View Controller Delegate
+- (void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller
+                       didAuthorizePayment:(PKPayment *)payment
+                                completion:(void (^)(PKPaymentAuthorizationStatus))completion {
+    
+    NSLog(@"Payment was authorized: %@", payment);
+    
+    NSLog(@"%@", payment.token);
+    
+    BOOL asyncSuccessful = NO;
+    
+    if (asyncSuccessful) {
+        completion(PKPaymentAuthorizationStatusSuccess);
+        
+        NSLog(@"Apple Pay支付成功");
+    } else {
+        
+        completion(PKPaymentAuthorizationStatusFailure);
+        
+        NSLog(@"Apple Pay支付失败");
+    }
+}
+
+- (void)paymentAuthorizationViewControllerDidFinish:(PKPaymentAuthorizationViewController *)controller {
+    
+    NSLog(@"paymentAuthorizationViewControllerDidFinish");
+    
+    [controller dismissViewControllerAnimated:YES
+                                   completion:nil];
 }
 
 @end
